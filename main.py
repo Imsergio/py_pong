f19bbf946a100de026a33ae9c044d042f3595ed3
UHSER <uhmarquez7@gmail.com>
2024-05-06 02:33:34 +0000

    import Turtle class

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world.")
+from Turtle import Screen
+
+screen = Screen()
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.setup(width=800, height=600)
+
+screen.exitonclick()
+
